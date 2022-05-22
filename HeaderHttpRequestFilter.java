802e355e3b04961e57e840b934a12264bfa1bb5c
lzd <vintandaLzd@163.com>
2022-05-23 00:26:42 +0800

    filter

---
+++ HeaderHttpRequestFilter.java
@@ -0,0 +1,15 @@
+package week03.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class HeaderHttpRequestFilter implements HttpRequestFilter {
+
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        String user = fullRequest.headers().get("user");
+        if (null == user || "".equals(user)) {
+            fullRequest.headers().set("user", "visitor");
+        }
+    }
+}
